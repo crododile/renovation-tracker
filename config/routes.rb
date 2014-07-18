ae9e7747ef482101445ec132f78049d4ddf19832
Coburn Berry <coburn.d.berry@gmail.com>
2014-07-17 21:48:10 -0500

    trying 12factor gem for static assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'inspections#form'
+  root 'inspections#new'
   
   resources :inspectors
 
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   resources :properties
   
+  #for poplulating dropdown
   get 'property_units/:id' => 'properties#units'
   
 
