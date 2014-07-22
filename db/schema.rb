eeb5c1bbe5466f3a78c263e1486ab5866b9de59e
Coburn Berry <coburn.d.berry@gmail.com>
2014-07-22 10:47:43 -0500

    tideid tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140721012442) do
+ActiveRecord::Schema.define(version: 20140722153950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,11 +69,6 @@ ActiveRecord::Schema.define(version: 20140721012442) do
     t.string   "twophaserehab"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "properties_currentunits", id: false, force: true do |t|
-    t.integer "properties_id"
-    t.integer "units_id"
   end
 
 end
