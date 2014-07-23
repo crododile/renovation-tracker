af3a814f2851c7f23db04da3c693c10b264b7309
Coburn Berry <coburn.d.berry@gmail.com>
2014-07-23 12:04:59 -0500

    line break

--- app/assets/javascripts/fileUploadUi.js
+++ app/assets/javascripts/fileUploadUi.js
@@ -1,7 +1,7 @@
 document.addEventListener("page:change", function () {	
 	var imageField = document.getElementById('image-field');
 	imageField.onchange = function () {
-		$("#image-field").append(
+		$('#image-field').append("<br>").append(
 			$('<input accept="image/*" id="inspection_image" multiple="multiple" name="inspection[image][]" type="file">')
 			)}
 });
