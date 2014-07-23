9c9f5f408737220702088c848703f40f1f496854
Coburn Berry <coburn.d.berry@gmail.com>
2014-07-22 22:32:29 -0500

    pictures save well

--- app/models/inspection_image.rb
+++ app/models/inspection_image.rb
@@ -13,4 +13,9 @@
 #
 
 class InspectionImage < ActiveRecord::Base
+  has_attached_file :inspection_photo, :styles => {
+        :big => "600x600>",
+        :small => "50x50#"
+      }
+validates_attachment_content_type :inspection_photo, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
