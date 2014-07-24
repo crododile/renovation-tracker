f2fa71fdd5cd9c78252168c062887844f76d3d7a
Coburn Berry <coburn.d.berry@gmail.com>
2014-07-23 22:08:52 -0500

    env var was wrong

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
     :url =>':s3_domain_url',                                      
     :path => '/:class/:attachment/:id_partition/:style/:filename',
     :s3_credentials => {                                          
-      :bucket => ENV['AWS_BUCKET'], #these values safely stored in application.yml thanks to figaro!                
+      :bucket => ENV['S3_BUCKET_NAME'], #these values safely stored in application.yml thanks to figaro!                
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],                 
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']          
     }                                                             
