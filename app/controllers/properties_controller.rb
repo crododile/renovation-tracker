5410576661331ebe0b64201f4a2df4e8cd42417a
Coburn Berry <coburn.d.berry@gmail.com>
2014-08-13 18:18:54 -0500

    comparisons creep forward

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -3,17 +3,34 @@ class PropertiesController < ApplicationController
   before_action :set_property, only: [:compare, :show, :edit, :update, :destroy, :units]
 
   def compare
-    unit_numbers = @property.unit_numbers.to_set
+    @unit_numbers = @property.unit_numbers.to_set
     @manager_inspections = @property.manager_inspections.to_a
     @inspections = @property.inspections.to_a
-    
-    debugger
+    @stack = []
     filter = Proc.new do |arr|
-      arr.select!{|inspection| unit_numbers.include? inspection.unit_number }
+      arr.select!{|inspection| @unit_numbers.include? inspection.unit_number }
     end
     
     filter.call @manager_inspections
     filter.call @inspections
+    
+    collector = Proc.new do |arr, num, collection|
+      if arr[0] && arr[0].unit_number == num
+        collection << arr.shift
+      else
+        collection << {unit_number: num, inspections: "0"}
+      end
+    end
+    
+    @unit_numbers.each do |num|
+      hereArr = []
+      hereArr << num
+      collector.call(@inspections, num, hereArr)
+      collector.call(@manager_inspections, num, hereArr)
+      @stack << hereArr 
+    end
+    
+    
     render "compare"
   end
 
