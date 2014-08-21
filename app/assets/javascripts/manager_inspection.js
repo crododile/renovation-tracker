260faa2537345bae62bba0b11cf67cb435280bc3
Coburn Berry <coburn.d.berry@gmail.com>
2014-08-21 12:06:10 -0500

    no more global, goes back to white on uncheck

--- app/assets/javascripts/manager_inspection.js
+++ app/assets/javascripts/manager_inspection.js
@@ -3,15 +3,21 @@ var manager_inspection_scripts = function () {
 	
 	$(".edit_manager_inspection").on("change", 
 	"input:checkbox", function (event) {
-		var $form = $(event.target.parentElement.parentElement)
-		$containingDiv = $(event.target.parentElement)
-		var resp = $.ajax({
+		var $form = $(event.target.parentElement.parentElement);
+		var $containingDiv = $(event.target.parentElement);
+		var myfunc = function(data){
+			$containingDiv.removeClass("matches");
+			$containingDiv.removeClass("mismatch");
+			$containingDiv.addClass(data["css"])
+		};
+		
+		$.ajax({
 			type: "POST",
 			url: $form.attr("action"), 
 			data: $form.serialize(), 
 			dataType: "JSON",
 			success: function (data) {
-				$containingDiv.addClass(data["css"])
+				myfunc(data)
 			},
 			error: function(data){
 				console.log("error")
